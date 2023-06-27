e09e52ceda167a925c24d83bef7c9553e13c8c69
thwjr5 <121143811+thwjr5@users.noreply.github.com>
2023-06-26 23:40:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,102 +1,53 @@
-//app.js
-
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
+app.use(express.json());
 app.use(express.static('public'));
-app.use('/videos', express.static('videos')); // Serve video files from the 'videos' folder
-
-app.get('/categories', (req, res) => {
-    const videoRootDirectory = path.join(__dirname, 'videos');
-
-    fs.readdir(videoRootDirectory, { withFileTypes: true }, (err, folders) => {
-        if (err) {
-            res.status(500).send('Error reading categories directory');
-            return;
-        }
-
-        const categories = folders.filter(folder => folder.isDirectory())
-                                  .map(folder => folder.name);
-        res.json(categories);
-    });
-});
+app.get('/videos/:videoName', function(req, res) {
+    const path = 'videos/' + req.params.videoName
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+  
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
+  
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+  
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
 
 app.get('/videos', (req, res) => {
-    const videoRootDirectory = path.join(__dirname, 'videos');
-    const start = parseInt(req.query.start) || 0;
-    const batchSize = 10;
-    const category = req.query.category || 'all';
-    const search = req.query.search || '';
-
-    fs.readdir(videoRootDirectory, { withFileTypes: true }, (err, folders) => {
-        if (err) {
-            res.status(500).send('Error reading video directory');
-            return;
-        }
-
-        const videoDirectories = folders.filter(folder => folder.isDirectory()).map(folder => folder.name);
-
-        const getVideoFiles = (dir, isRoot = false) => {
-            return fs.promises.readdir(dir, { withFileTypes: true })
-                .then(files => {
-                    const videoFiles = files.filter(file => file.isFile() && path.extname(file.name) === '.mp4')
-                        .map(file => ({ url: `/videos${isRoot ? '' : '/' + path.basename(dir)}/${file.name}`, name: file.name }));
-                    return videoFiles;
-                });
-        };
-        
-        const videoPromises = [getVideoFiles(videoRootDirectory, true), ...videoDirectories.map(dir => getVideoFiles(path.join(videoRootDirectory, dir)))];
-        
-        Promise.all(videoPromises)
-            .then((allVideos) => {
-                const videos = allVideos.flat();
-
-                let filteredVideos = videos;
-                if (category !== 'all') {
-                    filteredVideos = videos.filter(video => path.dirname(video.url).includes(category));
-                }
-
-                if (search) {
-                    const searchRegex = new RegExp(search, 'i');
-                    filteredVideos = filteredVideos.filter(video => searchRegex.test(video.name));
-                }
-
-                const videoBatch = filteredVideos.slice(start, start + batchSize);
-                res.json(videoBatch);
-            })
-            .catch((error) => {
-                res.status(500).send('Error processing video files');
-                console.error(error);
-            });
-    });
-});
-
-app.post('/move-video', (req, res) => {
-    const videoUrl = req.query.videoUrl;
-    const category = req.query.category;
-
-    if (!videoUrl || !category) {
-        res.status(400).send('Invalid parameters');
-        return;
+  fs.readdir('videos', (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    } else {
+      res.json(files);
     }
-
-    const videoPath = path.join(__dirname, videoUrl.substring(1));
-    const newPath = path.join(__dirname, 'videos', category, path.basename(videoUrl));
-
-    fs.promises.rename(videoPath, newPath)
-        .then(() => {
-            res.sendStatus(200);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send('Error moving video');
-        });
+  });
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -3,14 +3,12 @@
 const videoGrid = document.getElementById('video-grid');
 const sentinel = document.createElement('div');
 const searchBar = document.getElementById('search-bar');
-const categories = document.getElementById('categories');
 const overlay = document.getElementById('overlay');
 videoGrid.appendChild(sentinel);
 
 let videoStart = 0;
 const videoBatchSize = 10;
 let isInTheaterMode = false;
-let currentCategory = 'all';
 let searchTerm = '';
 
 function playVideo(event) {
@@ -25,208 +23,88 @@ function pauseVideo(event) {
   }
 }
 
-async function fetchCategories() {
+async function createVideoElement(videoFile) {
+  const videoContainer = document.createElement('div');
+  videoContainer.classList.add('video-container');
+
+  const videoElement = document.createElement('video');
+  videoElement.classList.add('video');
+  videoElement.src = `/videos/${videoFile.url.split('/').pop()}`;
+  videoElement.controls = false;
+  videoElement.muted = true;
+  videoElement.loop = true;
+  videoElement.setAttribute('preload', 'metadata');
+
+  videoContainer.appendChild(videoElement);
+  videoGrid.appendChild(videoContainer);
+
+  videoElement.addEventListener('mouseenter', playVideo);
+  videoElement.addEventListener('mouseleave', pauseVideo);
+
+  videoElement.addEventListener('click', () => {
+    toggleTheaterMode(videoElement);
+  });
+
+  return videoContainer;
+}
+
+// Fetch videos function
+async function fetchVideos(start = 0, search = '') {
   try {
-    const response = await fetch('/categories');
+    const response = await fetch(`/videos?start=${start}&search=${search}`);
     if (!response.ok) {
-      throw new Error('Error fetching categories');
+      throw new Error('Error fetching video list');
     }
-    return response.json();
+    const videoFiles = await response.json();
+    for (const videoFile of videoFiles) {
+      const videoElement = await createVideoElement(videoFile);
+      videoGrid.appendChild(videoElement);
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
-async function createCategorySelect() {
-  try {
-    const categories = await fetchCategories();
-    const select = document.createElement('select');
-
-    const defaultOption = document.createElement('option');
-    defaultOption.value = '';
-    defaultOption.textContent = 'Select Category';
-    select.appendChild(defaultOption);
-
-    categories.forEach((category) => {
-      const option = document.createElement('option');
-      option.value = category;
-      option.textContent = category;
-      select.appendChild(option);
-    });
-
-    return select;
-  } catch (error) {
-    console.error(error);
+function toggleTheaterMode(video) {
+  if (video.classList.contains('theater-mode')) {
+    video.classList.remove('theater-mode');
+    overlay.style.display = 'none';
+    video.muted = true;
+    video.controls = false;
+    isInTheaterMode = false;
+    video.addEventListener('mouseover', playVideo);
+    video.addEventListener('mouseout', pauseVideo);
+  } else {
+    video.classList.add('theater-mode');
+    overlay.style.display = 'block';
+    video.muted = false;
+    video.controls = false;
+    video.play();
+    isInTheaterMode = true;
+    video.removeEventListener('mouseover', playVideo);
+    video.removeEventListener('mouseout', pauseVideo);
   }
 }
 
-function moveVideoToCategory(videoElement, videoUrl, category) {
-  fetch(`/move-video?videoUrl=${videoUrl}&category=${category}`, {
-    method: 'POST',
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Error moving video');
-      }
-      videoElement.remove();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-
-// Create the category elements
-function createCategoryElement(category) {
-  const categoryElement = document.createElement('div');
-  categoryElement.classList.add('category');
-  categoryElement.textContent = category;
-  categoryElement.dataset.category = category;
-
-  if (category === 'all') {
-    categoryElement.classList.add('active');
-  }
-
-  return categoryElement;
-}
-
-// Initialize categories
-async function initializeCategories() {
-    const categoryList = await fetchCategories();
-    categoryList.unshift('all'); // Add 'all' category to the beginning of the array
-  
-    categoryList.forEach((category) => {
-      const categoryElement = createCategoryElement(category);
-      categories.appendChild(categoryElement);
-    });
-  }
-  
-  initializeCategories();
-  
-  categories.addEventListener('click', (event) => {
-    const categoryElement = event.target.closest('.category');
-    if (categoryElement) {
-      const category = categoryElement.dataset.category;
-      if (currentCategory !== category) {
-        currentCategory = category;
-        videoStart = 0;
-        videoGrid.innerHTML = ''; // Clear the current video grid
-        fetchVideos(videoStart, currentCategory, searchTerm);
-        Array.from(categories.children).forEach((el) =>
-          el.classList.remove('active')
-        );
-        categoryElement.classList.add('active');
-      }
+function observerCallback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      videoStart += videoBatchSize;
+      fetchVideos(videoStart, searchTerm);
+      observer.disconnect();
+      videoGrid.removeChild(sentinel);
+      videoGrid.appendChild(sentinel);
+      observer.observe(sentinel);
     }
   });
-  
-  async function createVideoElement(videoFile) {
-    const videoContainer = document.createElement('div');
-    videoContainer.classList.add('video-container');
-  
-    const videoElement = document.createElement('video');
-    videoElement.classList.add('video');
-    videoElement.src = videoFile.url;
-    videoElement.controls = false;
-    videoElement.muted = true;
-    videoElement.loop = true;
-    videoElement.setAttribute('preload', 'metadata');
-  
-    const categorySelect = await createCategorySelect();
-    categorySelect.addEventListener('change', () => {
-      moveVideoToCategory(
-        videoContainer,
-        videoFile.url,
-        categorySelect.value
-      );
-    });
-  
-    videoContainer.appendChild(videoElement);
-    videoContainer.appendChild(categorySelect);
-  
-    const videoSelectCheckbox = document.createElement('input');
-    videoSelectCheckbox.type = 'checkbox';
-    videoSelectCheckbox.classList.add('video-select-checkbox');
-    videoContainer.appendChild(videoSelectCheckbox);
-  
-    videoElement.addEventListener('mouseenter', playVideo);
-    videoElement.addEventListener('mouseleave', pauseVideo);
-  
-    videoElement.addEventListener('click', () => {
-      toggleTheaterMode(videoElement);
-    });
-  
-    return videoContainer;
-  }
-  
-  // Fetch videos function
-  async function fetchVideos(start = 0, category = 'all', search = '') {
-    try {
-      const response = await fetch(`/videos?start=${start}&category=${category}&search=${search}`);
-      if (!response.ok) {
-        throw new Error('Error fetching video list');
-      }
-      const videoFiles = await response.json();
-      for (const videoFile of videoFiles) {
-        const videoElement = await createVideoElement(videoFile);
-        videoGrid.appendChild(videoElement);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  function toggleTheaterMode(video) {
-    if (video.classList.contains('theater-mode')) {
-      video.classList.remove('theater-mode');
-      overlay.style.display = 'none';
-      video.muted = true;
-      video.controls = false;
-      isInTheaterMode = false;
-      video.addEventListener('mouseover', playVideo);
-      video.addEventListener('mouseout', pauseVideo);
-    } else {
-      video.classList.add('theater-mode');
-      overlay.style.display = 'block';
-      video.muted = false;
-      video.controls = false;
-      video.play();
-      isInTheaterMode = true;
-      video.removeEventListener('mouseover', playVideo);
-      video.removeEventListener('mouseout', pauseVideo);
-    }
-  }
-  
-  function observerCallback(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        videoStart += videoBatchSize;
-        fetchVideos(videoStart, currentCategory, searchTerm);
-        observer.disconnect();
-        videoGrid.removeChild(sentinel);
-        videoGrid.appendChild(sentinel);
-        observer.observe(sentinel);
-      }
-    });
-  }
-  
-  const observer = new IntersectionObserver(observerCallback, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0,
-  });
-  
-  observer.observe(sentinel);
-  
-  fetchVideos(videoStart, currentCategory, searchTerm);
-  
-  searchBar.addEventListener(
-    'input',
-    debounce((event) => {
-      searchTerm = event.target.value.trim();
-      videoStart = 0;
-      videoGrid.innerHTML = '';
-      fetchVideos(videoStart, currentCategory, searchTerm);
-    }, 500)
-  );
-  
-  
+}
+
+const observer = new IntersectionObserver(observerCallback, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
+});
+
+observer.observe(sentinel);
+
+fetchVideos(videoStart, searchTerm);
